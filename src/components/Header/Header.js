f6e2ac9aab9d367c129bcd70d7793d9d4c1bf92f
Aminat.A <aminatadesola@gmail.com>
2020-06-26 16:00:59 -0500

    added gif to repo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header(){
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
     <h1 class="display-4">Employee Directory</h1>
-    <p class="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+    <p class="lead">Use the search box to narrow your results.</p>
     </div>
 </div>
 );
