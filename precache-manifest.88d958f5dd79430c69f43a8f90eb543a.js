79c6339a083af20258ec2307ede5f05d96e0abb9
Aminat.A <aminatadesola@gmail.com>
2020-06-26 13:57:37 -0500

    Updates

---
+++ precache-manifest.88d958f5dd79430c69f43a8f90eb543a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a513e535a15177c08af64c03c8868bde",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "b93ba76c0a5c97267036",
+    "url": "/user-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21f2a378fe13a88529c9",
+    "url": "/user-directory/static/js/2.310764bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.310764bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93ba76c0a5c97267036",
+    "url": "/user-directory/static/js/main.c2234c8c.chunk.js"
+  },
+  {
+    "revision": "83e5886700bf411ff314",
+    "url": "/user-directory/static/js/runtime-main.f31256e8.js"
+  }
+]);
