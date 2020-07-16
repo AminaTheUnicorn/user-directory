39bfc5829c8e6fba1e98b1b23080c9282ac8b991
Aminat.A <aminatadesola@gmail.com>
2020-07-16 16:46:28 -0500

    header color changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import "../Header/styles/Header.css";
 function Header(){
 
     return (
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-    <h1 class="display-4">Employee Directory</h1>
-    <p class="lead">Use the search box to narrow your results.</p>
+    <div class="jumbotron jumbotron-fluid bg-info" style={{color:"white"}}>
+        <div class="container">
+            <h1 class="display-4">Employee Directory</h1>
+            <p class="lead">Use the search box to narrow your results.</p>
+        </div>
     </div>
-</div>
 );
     }
 export default Header;
